6e97e2810af963725cc5ee8ed714696adca05a82
Prayas Patel <Prayas Patel@inindcrws.com>
2026-06-16 13:59:32 +0530

    add compose

--- killercoda.sh
+++ killercoda.sh
@@ -5,3 +5,7 @@ cd docker-task-management
 
 git init 
 git pull https://github.com/prayasoncloud/Task-Management-System-Docker.git
+cd backend
+docker build -t py-image .
+docker run -d -p 5000:5000 py-image:latest
+
