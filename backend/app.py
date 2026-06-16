667d5715ff0db8713ccd2529f2de642d8c31d5d6
Prayas Patel <Prayas Patel@inindcrws.com>
2026-06-16 14:17:28 +0530

    Add def db_check():

--- backend/app.py
+++ backend/app.py
@@ -16,3 +16,22 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+@app.route("/db")
+def db_check():
+    conn = psycopg2.connect(
+        host="postgres",
+        database="taskdb",
+        user="admin",
+        password="password"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+
+    version = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return str(version)
